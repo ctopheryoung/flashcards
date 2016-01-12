d72d76328691b0dba99b72359778cb5ed7563ccb
Chris Young and Trevor Elvey <trevor.elvey@gmail.com>
2016-01-11 16:42:00 -0800

    added well styling, jQuery functions and first 4 slides

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(function() {
+/*  $("#clickable").click(function() {
+    $(".topic-shown").toggle();
+    $(".topic-hidden").toggle();
+  });
+*/
+  $("#clickable1").click(function() {
+    $("#topic-shown1").toggle();
+    $("#topic-hidden1").toggle();
+  });
+
+  $("#clickable2").click(function() {
+    $("#topic-shown2").toggle();
+    $("#topic-hidden2").toggle();
+  });
+
+  $("#clickable3").click(function() {
+    $("#topic-shown3").toggle();
+    $("#topic-hidden3").toggle();
+  });
+
+  $("#clickable4").click(function() {
+    $("#topic-shown4").toggle();
+    $("#topic-hidden4").toggle();
+  });
+
+/*
+ $(".well").click(function() {
+   console.log("yay cats!");
+   console.log(this.id);
+   $("#topic-shown" + this.id).toggle();
+   $("#topic-hidden" + this.id).toggle();
+ });
+*/
+});
